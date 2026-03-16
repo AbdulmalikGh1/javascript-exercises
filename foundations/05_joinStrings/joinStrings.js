3ed823d3659b66c34fe21e5d780ad9c09717ed2d
Abdulmalik Ghazi <451110212@qu.edu.sa>
2026-03-16 05:38:29 +0300

    Finshed first leason

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,11 +7,20 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+const { f } = require("../04_mathEquations/mathEquations")
+
 // Add your code right below, good luck!
+let firstName = "Carlos"
+let lastName = "Stevenson"
+let thisYear = 1965
+let birthYear = 1947
 
+let fullName = firstName + " " + lastName
+let age = thisYear - birthYear
 
+let greeting = "Hello! My name is " + fullName + " and I am " + age + " years old." 
 
-
+console.log(greeting)
 
 // Do not change this
 module.exports = {
